d3034c9bf3d859e61ddfe7c20aeb16be6d9d5e3a
Dipti Patil <diptipatil77@gmail.com>
2026-07-24 02:24:16 +0530

    Fix unused import

--- src/utils/formatRelativeTime.ts
+++ src/utils/formatRelativeTime.ts
@@ -1,5 +1,5 @@
 import { formatDistanceToNow } from 'date-fns';
-import { cn } from '@/utils';
+
 
 export function formatRelativeTime(date: string | Date): string {
   return formatDistanceToNow(new Date(date), { addSuffix: true });
